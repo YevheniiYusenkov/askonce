f8601240564ab55de4395a226794f6a52b85de88
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-07-29 23:41:22 +0300

    Added ask, answer Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AskModule } from './ask/ask.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [],
+  imports: [AskModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ask/ask.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('ask')
+export class AskController {}
