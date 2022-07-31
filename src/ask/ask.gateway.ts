1b4e27460a75989eb761dd578f0d3f1fcfb2065b
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-07-31 13:15:42 +0300

    Update ask.gateway.ts

--- src/ask/ask.gateway.ts
+++ src/ask/ask.gateway.ts
@@ -1,18 +1,25 @@
+import { Socket, Server } from 'socket.io';
 import {
-  ConnectedSocket,
-  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'net';
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @WebSocketGateway({ namespace: 'ask' })
-export class AskGateway {
-  @SubscribeMessage('ask')
-  handleMessage(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() payload: any,
-  ): string {
-    return 'Hello world!';
-  }
+export class AskGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async handleConnection(client: Socket): Promise<void> {}
+
+  async handleDisconnect(client: Socket): Promise<void> {}
+
+  @SubscribeMessage('message')
+  async handleMessage(client: Socket, message: string): Promise<void> {}
 }
