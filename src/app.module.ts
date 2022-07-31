2b12a26a3a142f093d3327b12deace2ca428dd61
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-07-31 13:15:29 +0300

    Implemented cache-manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AskModule } from './ask/ask.module';
 import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [AskModule, AnswerModule],
+  imports: [
+    AskModule,
+    AnswerModule,
+    CacheModule.register({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
